ae7ba8bb16865d3024c50c9740ddafd932329ffe
onehowon <flash0221@ajou.ac.kr>
2023-09-05 15:29:12 +0900

    0905

--- chat-bot/common.js
+++ chat-bot/common.js
@@ -66,31 +66,81 @@ function getCookie(cookieName){
     return unescape(cookieValue);
 }
 
-$(function(){
-    // id 저장 체크 박스 기능 추가
-    var userInputId = getCookie("userInputId");
-    $("manager_id").val(userInputId);
 
-    if($("#manager_id").val() != ""){ // 그 전에 id 저장해서 처음 페이지 로딩
-        $("#save-id").attr("checked", true); // ID 저장하기를 체크 상태로 두기
+// $(function(){
+//     // id 저장 체크 박스 기능 추가
+//     var userInputId = getCookie("userInputId");
+//     $("manager_id").val(userInputId);
+
+//     if($("#manager_id").val() != ""){ // 그 전에 id 저장해서 처음 페이지 로딩
+//         $("#save-id").attr("checked", true); // ID 저장하기를 체크 상태로 두기
+//     }
+
+//     $("save-id").change(function(){ // 체크박스에 변화가 발생시
+//         if($("#save-id").is(":chekced")){ // id 저장하기 체크했을 때
+//             var userInputId = $("#manager_id").val();
+//             setCookie("userInputId", userInputId, 7); // 7일간 쿠키 보관
+//         } else{ // ID 저장하기 체크 해제 시
+//             deleteCookie("userInputId");
+//         }
+//     });
+
+//     // ID 저장하기 체크한 상태에서 id 입력시 이럴때도 쿠키 저장
+//     $("#manager_id").keyup(function(){ // 입력 칸에 id를 입력시
+//         if($("#useCookie").is("checked")){
+//             var userInputId = $("#manager_id").val();
+//             setCookie("userInputId", userInputId, 7);
+//         }
+//     });
+// });
+
+// // ID 저장하기 알고리즘 -->
+
+// 자동 로그인 -->
+window.onload = function(){
+    const rememberMeCheckbox = document.getElementById('auto-login');
+    const loginForm = document.querySelector('form');
+
+    // 저장된 사용자 정보 가져오기
+    const savedUsername = localStorage.getItem('username');
+    const savedPassword = localStorage.getItem('password');
+    const savedAutoLogin = localStorage.getItem('autoLogin');
+
+    if(savedAutoLogin == 'true'){
+        // 자동 로그인 체크 한 경우, 저장된 아이디 / 비밀번호 입력란에 채우기
+        if(savedUsername && savedPassword){
+            document.getElementById('user-id').value = savedUsername;
+            document.getElementById('user-pw').value = savedPassword;
+        }
+
+        //로그인 시도
+        login();
     }
 
-    $("save-id").change(function(){ // 체크박스에 변화가 발생시
-        if($("#save-id").is(":chekced")){ // id 저장하기 체크했을 때
-            var userInputId = $("#manager_id").val();
-            setCookie("userInputId", userInputId, 7); // 7일간 쿠키 보관
-        } else{ // ID 저장하기 체크 해제 시
-            deleteCookie("userInputId");
-        }
-    });
+    // 로그인 버튼 클릭 시 실행되는 함수
+    function login(){
+        const username = document.getElementById('user-id').value;
+        const password = document.getElementById('user-pw').value;
 
-    // ID 저장하기 체크한 상태에서 id 입력시 이럴때도 쿠키 저장
-    $("#manager_id").keyup(function(){ // 입력 칸에 id를 입력시
-        if($("#useCookie").is("checked")){
-            var userInputId = $("#manager_id").val();
-            setCookie("userInputId", userInputId, 7);
-        }
-    });
-});
+        // 로그인 로직을 여기에 추가
+        // 원래는 회원 DB 연결하여 사용자 정보를 받아와야 하나, 아무 사용자나 성공한 것으로 간주
 
-// ID 저장하기 알고리즘 -->
+        // 로그인 성공 시, 자동로그인 정보 저장
+        if(rememberMeCheckbox.checked){
+            localStorage.setItem('username', username);
+            localStorage.setItem('password', password);
+            localStorage.setItem('autoLogin', 'true');
+        } else{
+            localStorage.removeItem('username');
+            localStorage.removeItem('password');
+            localStorage.setItem('autoLogin', 'false');
+        }
+
+        // 실제 로그인 로직 추가할 부분
+        // 로그인에 성공하면 다음 페이지로 리다이렉션 할 부분은 이미 login() 에 구현되어 있음
+    }
+};
+
+function logout(){
+    history.go(-1);
+}
