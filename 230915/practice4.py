cc5cb86fe7fb86fb6a2fcd5880e38b0f1d1fb6ef
onehowon <flash0221@ajou.ac.kr>
2023-09-15 13:43:28 +0900

    0915

--- 230915/practice4.py
+++ 230915/practice4.py
@@ -1,20 +1,40 @@
 import random
 
+
 class Account:
-  def __init__(self, name, price):
-    self.name = name
-    self.price = price
-    self.bank = "SC제일은행"
-    num1 = random.randint(0, 999)
-    num2 = random.randint(0, 99)
-    num3 = random.randint(0, 999999)
+    # class variable
+    account_count = 0
 
-    num1 = str(num1).zfill(3)
-    num2 = str(num2).zfill(2)
-    num3 = str(num3).zfill(6)
+    def __init__(self, name, balance):
+        self.name = name
+        self.balance = balance
+        self.bank = "SC은행"
 
-    self.account_number = num1 + '-' + num2 + '-' + num3
+        # 3-2-6
+        num1 = random.randint(0, 999)
+        num2 = random.randint(0, 99)
+        num3 = random.randint(0, 999999)
 
-kim = Account("김민수", 100)
+        num1 = str(num1).zfill(3)  # 1 -> '1' -> '001'
+        num2 = str(num2).zfill(2)  # 1 -> '1' -> '01'
+        num3 = str(num3).zfill(6)  # 1 -> '1' -> '0000001'
+        self.account_number = num1 + '-' + num2 + '-' + num3  # 001-01-000001
+        Account.account_count += 1
 
-print(kim.bank)
+    @classmethod
+    def get_account_num(cls):
+        print(cls.account_count)  # Account.account_count
+
+    def deposit(self, amount):
+        if amount >= 1:
+            self.balance += amount
+
+    def withdraw(self, amount):
+        if self.balance > amount:
+            self.balance -= amount
+
+k = Account("kim", 100)
+k.deposit(100)
+k.withdraw(90)
+print(k.balance)
+
