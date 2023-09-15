9530e5bc3240220d7b019b9e3c9b139e2c2c8af0
onehowon <flash0221@ajou.ac.kr>
2023-09-15 13:59:39 +0900

    0915

---
+++ 230915/practice5.py
@@ -0,0 +1,72 @@
+import random
+
+
+class Account:
+    # class variable
+    account_count = 0
+
+    def __init__(self, name, balance):
+        self.deposit_count = 0
+        self.deposit_log = []
+        self.withdraw_log = []
+
+        self.name = name
+        self.balance = balance
+        self.bank = "SC은행"
+
+        # 3-2-6
+        num1 = random.randint(0, 999)
+        num2 = random.randint(0, 99)
+        num3 = random.randint(0, 999999)
+
+        num1 = str(num1).zfill(3)  # 1 -> '1' -> '001'
+        num2 = str(num2).zfill(2)  # 1 -> '1' -> '01'
+        num3 = str(num3).zfill(6)  # 1 -> '1' -> '0000001'
+        self.account_number = num1 + '-' + num2 + '-' + num3  # 001-01-000001
+        Account.account_count += 1
+
+    @classmethod
+    def get_account_num(cls):
+        print(cls.account_count)  # Account.account_count
+
+    def deposit(self, amount):
+        if amount >= 1:
+            self.deposit_log.append(amount)
+            self.balance += amount
+
+            self.deposit_count += 1
+            if self.deposit_count % 5 == 0:         # 5, 10, 15
+                # 이자 지금
+                self.balance = (self.balance * 1.01)
+
+
+    def withdraw(self, amount):
+        if self.balance > amount:
+            self.withdraw_log.append(amount)
+            self.balance -= amount
+
+    def display_info(self):
+        print("은행이름: ", self.bank)
+        print("예금주: ", self.name)
+        print("계좌번호: ", self.account_number)
+        print("잔고: ", self.balance)
+
+    def withdraw_history(self):
+        for amount in self.withdraw_log:
+            print(amount)
+
+    def deposit_history(self):
+        for amount in self.deposit_log:
+            print(amount)
+
+
+k = Account("Kim", 1000)
+k.deposit(100)
+k.deposit(200)
+k.deposit(300)
+k.deposit_history()
+
+k.withdraw(100)
+k.withdraw(200)
+k.withdraw_history()
+
