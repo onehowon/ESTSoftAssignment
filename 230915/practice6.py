4a0c68f596261ef33305a5da522fa05bce6e487e
onehowon <flash0221@ajou.ac.kr>
2023-09-15 14:23:44 +0900

    0915

---
+++ 230915/practice6.py
@@ -0,0 +1,25 @@
+class 차:
+    def __init__(self, 바퀴, 가격):
+        self.바퀴 = 바퀴
+        self.가격 = 가격
+
+    def 정보(self):
+        print("바퀴수 ", self.바퀴)
+        print("가격 ", self.가격)
+
+class 자동차(차):
+    def __init__(self, 바퀴, 가격):
+        super().__init__(바퀴, 가격)
+
+class 자전차(차):
+    def __init__(self, 바퀴, 가격, 구동계):
+        super().__init__(바퀴, 가격)
+        self.구동계 = 구동계
+
+    def 정보(self):
+      super().정보()
+      print("구동계: ", self.구동계)
+
+bicycle = 자전차(2, 100, "시마노")
+bicycle.정보()
+
