31f3e2652b4e9aa0b5c5126bcd262696296f75b7
onehowon <flash0221@ajou.ac.kr>
2023-10-05 10:17:57 +0900

    1005

--- myapp/app/urls.py
+++ myapp/app/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 
 
 urlpatterns = [
-	path("admin/", admin.site.urls),
-	path('blog/', include("blog.urls")),
+    path('polls/', include('polls.urls')),
+    path('admin/', admin.site.urls),
 ]
