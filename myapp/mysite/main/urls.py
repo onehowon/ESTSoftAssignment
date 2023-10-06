6b9f22737cd56707221ae70f8785cae08682fb98
onehowon <flash0221@ajou.ac.kr>
2023-10-06 16:50:18 +0900

    1006

--- myapp/mysite/main/urls.py
+++ myapp/mysite/main/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
-    path('test/', views.test, name='test')
+    path('test/', views.test, name='test'),
 ]
--- myapp/mysite/main/views.py
+++ myapp/mysite/main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.template.loader import render_to_string
 from django.http import HttpResponse
+from django.template.loader import render_to_string
 
 def index(request):
     return render(request, 'main/index.html')
@@ -13,13 +13,14 @@ def contact(request):
 
 def test(request):
     data = [
-        {'name' : 'leehojun', 'age': 10},
-        {'name' : 'leehojun2', 'age' : 20}
+        {'name': 'leehojun', 'age':10},
+        {'name': 'leehojun2', 'age':20}
     ]
-    
-    s = render_to_string('main/test.txt', {'data' : data})
+    # return HttpResponse('hello world') # 1
+    # return HttpResponse('<h1>hello world</h1>') # 2
+    # 결국 render도 HttpResponse를 리턴합니다. :)
+    s = render_to_string('main/test.txt', {'data': data})
     header = '<h2>hello world</h2>'
     footer = '<h2>bye world</h2>'
-    return HttpResponse(header + s + footer)
-
-# Create your views here.
+    return HttpResponse(header + s + footer) # 3
+    # return render(request, 'main/test.html')
