1a54f4c3e258c1738cbb7448f7ad98595d97c5c3
onehowon <flash0221@ajou.ac.kr>
2023-10-06 15:59:31 +0900

    1006

--- myapp/mysite/blog/admin.py
+++ myapp/mysite/blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Post, Category, Tag, Comment
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields={'slugs':('name',)}
+    
+class TagAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+    
+admin.site.register(Post)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Comment)
+
 
 # Register your models here.
