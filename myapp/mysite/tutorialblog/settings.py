3eeaee69d2e76d39b14f6b26d6232f588612fd05
onehowon <flash0221@ajou.ac.kr>
2023-10-10 14:24:59 +0900

    1010

--- myapp/mysite/tutorialblog/settings.py
+++ myapp/mysite/tutorialblog/settings.py
@@ -120,12 +120,16 @@ USE_I18N = True
 
 USE_TZ = False
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
+
 MEDIA_URL = '/media/'
+
 MEDIA_ROOT = BASE_DIR / 'media'
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
