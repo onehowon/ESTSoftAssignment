c2ea90aec3af5d09941b512e3e93cc44f46857c5
onehowon <flash0221@ajou.ac.kr>
2023-10-11 15:07:01 +0900

    1011

--- Django/app1/app1/settings.py
+++ Django/app1/app1/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'app1',
     'instagram',
+    'user'
 ]
 
 MIDDLEWARE = [
@@ -102,6 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'user.User'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
--- Django/app1/user/models.py
+++ Django/app1/user/models.py
@@ -1,3 +1,24 @@
+# ~/user/models.py
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+
 
 # Create your models here.
+class User(AbstractBaseUser):
+    name = models.CharField(max_length=30, blank=True, null=True)
+    email = models.EmailField(verbose_name='email', max_length=100, blank=True, null=True, unique=True)
+    user_id = models.CharField(max_length=30, blank=True, null=True)
+    thumbnail = models.CharField(max_length=256, default='default_profile.jpg', blank=True, null=True)
+
+    USERNAME_FIELD = 'id'
+    REQUIRED_FIELDS = ['user_id']
+
+    def __str__(self):
+        return self.user_id
+
+    @property
+    def is_staff(self):
+        return self.is_admin
+
+    class Meta:
+        db_table = 'users'
