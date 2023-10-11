9219a70445a44d1f7c82b4b9bccc85498a2230ff
onehowon <flash0221@ajou.ac.kr>
2023-10-11 13:27:24 +0900

    1011

--- Django/app1/app1/urls.py
+++ Django/app1/app1/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import Main
+from .views import Main, UploadFeed
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', Main.as_view()),
-    path('admin/', admin.site.urls),
+    path('instagram/upload', admin.site.urls),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
